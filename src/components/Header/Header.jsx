71a7d2f3d3df5c0fc528ed47f236cd4f18eb47c3
odistouchto <odis@touchto.io>
2023-01-22 21:24:47 -0600

    [PI 7.2] Grammar tweak

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -10,8 +10,8 @@ const Header = () => {
       </div>
       <div className="MobileHeaderContainer">
         <h1>Simple, traffic-based pricing</h1>
-        <p>Sign-up for our 30-day trial</p>
-        <p>. No credit card required.</p>
+        <p>Sign-up for our 30-day trial.</p>
+        <p>No credit card required.</p>
       </div>
     </div>
   );
